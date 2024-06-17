79de44d677194bf2d0a63a372d66e5e8948bbbf4
Ayush Yadav <sidharth197502@gmail.com>
2024-06-17 11:52:46 +0530

    Created SignUp.js

---
+++ JS/SignUp.js
@@ -0,0 +1,61 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
+import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
+import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+
+  const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    // Initialize Firebase Authentication,Database,Firestore
+    const auth = getAuth(app);
+    const database = getDatabase(app);
+    const firestore = getFirestore(app);
+
+    // Function to Sign up new users
+    const signup = async (name, email, password) => {
+      try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
+        console.log('User signed up:', user);
+    
+        // Saving additional user information to Realtime Database
+        await set(ref(database, 'users/' + user.uid), {
+          username: name,
+          email: email,
+        });
+    
+        // Saving additional user information to Firestore
+        await setDoc(doc(firestore, 'users', user.uid), {
+          username: name,
+          email: email,
+        });
+    
+        console.log('User data saved to Realtime Database and Firestore');
+      } catch (error) {
+        console.error('Error during sign up:', error.code, error.message);
+      }
+    };
+ 
+  document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('SignUpForm').addEventListener('submit', (event) => {
+      event.preventDefault();
+  
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+  
+      console.log(name, email, password); 
+      signup(name, email, password);
+    });
+  });
